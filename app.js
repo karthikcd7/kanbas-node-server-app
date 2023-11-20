6c24ee0a95b26c9cbccd827d1bd9a04f76f63e83
karthikcd7 <karthikcd7@gmail.com>
2023-11-20 12:49:11 -0500

    assignment 5 node server

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import "dotenv/config";
+import Hello from "./hello.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import cors from "cors";
+import Lab5 from "./lab5.js";
+const app = express();
+app.use(cors());
+app.use(express.json());
+CourseRoutes(app);
+ModuleRoutes(app);
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
