1e10670c488475c51d3e40d8d30f8b78df7531d3
karthikcd7 <karthikcd7@gmail.com>
2023-12-02 01:25:48 -0500

    added Users

--- Database/index.js
+++ Database/index.js
@@ -1,5 +1,5 @@
-import courses from "./courses.json"
-import modules from "./modules.json"
+import courses from "./courses.json" 
+import modules from "./modules.json" 
 import assignments from "./assignments.json"
 import users from "./users.json"
 import grades from "./grades.json"
