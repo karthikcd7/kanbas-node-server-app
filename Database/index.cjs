b426b0abed71050d452ae8e7750b30565f86f710
karthikcd7 <karthikcd7@gmail.com>
2023-12-02 23:56:24 -0500

    added index.cjs

---
+++ Database/index.cjs
@@ -0,0 +1,19 @@
+const courses = require('./courses.json');
+const modules = require('./modules.json');
+const assignments = require('./assignments.json');
+const users = require('./users.json');
+const grades = require('./grades.json');
+const enrollments = require('./enrollments.json');
+// import modules from "./modules.json" assert { type: "json" };
+// import assignments from "./assignments.json" assert { type: "json" };
+// import users from "./users.json" assert { type: "json" };
+// import grades from "./grades.json" assert { type: "json" };
+// import enrollments from "./enrollments.json" assert { type: "json" };
+module.exports = {
+  courses,
+  modules,
+  assignments,
+  users,
+  grades,
+  enrollments,
+};
